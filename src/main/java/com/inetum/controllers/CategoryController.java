c5a6467c33683896798791e654a86d402fc38631
NourAmine.Drissi <drissinouramine@gmail.com>
2021-10-05 15:30:42 +0200

    last build again

--- src/main/java/com/inetum/controllers/CategoryController.java
+++ src/main/java/com/inetum/controllers/CategoryController.java
@@ -24,7 +24,7 @@ public class CategoryController {
         this.categoryService = categoryService;
         this.modelMapper = modelMapper;
     }
-    //this is the last build
+    //this is the last build again
     @PostMapping(path = "/add-new-category")
     public  Category addNewCategry (@RequestBody CategoryDto categoryDto){
         Category category = modelMapper.map(categoryDto,Category.class);
